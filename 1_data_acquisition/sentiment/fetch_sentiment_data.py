83831896995fc3e4d886cc7743cf0f79273c74cc
Dominika Umiastowska <62251932+umiastowskad@users.noreply.github.com>
2022-11-27 20:36:21 +0100

    Update skryptu do sentymentu

--- 1_data_acquisition/sentiment/fetch_sentiment_data.py
+++ 1_data_acquisition/sentiment/fetch_sentiment_data.py
@@ -8,8 +8,13 @@ out_dir = 'data'
 
 
 def main():
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
+    os.environ['KAGGLE_USERNAME'] = "bdared"
+    os.environ['KAGGLE_KEY'] = "b47c5d207bf238ad6fbf29ba4b8cba58"
+
+    os.system('kaggle datasets download -d yash612/stockmarket-sentiment-dataset')
+
+    kaggle_dataset = pd.read_csv('./stockmarket-sentiment-dataset.zip', compression='zip')
+    os.remove('stockmarket-sentiment-dataset.zip')
 
     sentiment_reddit = pd.read_csv(sentiment_reddit_url)
     sentiment_twitter = pd.read_csv(sentiment_twitter_url)
@@ -17,11 +22,17 @@ def main():
     sentiment_reddit = sentiment_reddit.iloc[:, [0, 1]]
     sentiment_twitter = sentiment_twitter.iloc[:, [1, 2]]
 
-    sentiment_reddit.rename({'Comment Text': 'Subreddit'}, axis=1, inplace=True)
-    sentiment_twitter.rename({'Tweet Text': 'Subreddit'}, axis=1, inplace=True)
+    sentiment_reddit.rename({'Comment Text': 'Text'}, axis=1, inplace=True)
+    sentiment_twitter.rename({'Tweet Text': 'Text'}, axis=1, inplace=True)
 
     sentiment_data = pd.concat([sentiment_reddit, sentiment_twitter], axis=0)
+    sentiment_data.replace({"Positive": 1, "Negative": -1}, inplace=True)
+    sentiment_data = pd.concat([sentiment_data, kaggle_dataset], axis=0)
+
     sentiment_data.reset_index(inplace=True, drop=True)
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
     sentiment_data.to_csv(os.path.join(out_dir, 'sentiment_data.csv'), index=False)
 
