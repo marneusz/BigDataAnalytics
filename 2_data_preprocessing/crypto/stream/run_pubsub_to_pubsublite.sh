d958a5f5b8a728bd90db57d2effa264167ed7e54
marneusz <mateusz.wojcik12@gmail.com>
2023-01-28 00:45:37 +0000

    lite to bigtable update

--- 2_data_preprocessing/crypto/stream/run_pubsub_to_pubsublite.sh
+++ 2_data_preprocessing/crypto/stream/run_pubsub_to_pubsublite.sh
@@ -5,7 +5,7 @@ CLUSTER_ID="bda-hdfs"
 PROJECT_NUMBER="1072423212419"
 PUBSUBLITE_LOCATION=
 
-gcloud dataproc jobs submit pyspark pubsub_to_pubsublite.py \
+gcloud dataproc jobs submit pyspark 1_pubsub_to_pubsublite.py \
     --region=$DATAPROC_REGION \
     --cluster=$CLUSTER_ID \
     --jars=gs://spark-lib/pubsublite/pubsublite-spark-sql-streaming-LATEST-with-dependencies.jar \
